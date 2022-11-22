d6c172b33673bdcbadaecd91909a7055f97820cc
Daniel <dmforeacre@gmail.com>
2022-11-21 22:33:56 -0500

    Lighting tests

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private float lightMultiplier = 2;
+
     private float timeOfDay;
+    private Light sun;
 
     // Start is called before the first frame update
     void Start()
     {
         timeOfDay = 0;
 
+        sun = GameObject.Find("Directional Light").GetComponent<Light>();
+
         ToolData.Initialize();
-
         PlayerHandler.Initialize();
-
         Inventory.Initialize();
-
         UIManager.Initialize();
 
     }
@@ -30,6 +32,21 @@ public class GameManager : MonoBehaviour
             PlayerHandler.AddValue("health", 1);
             t = 0;
         }
+
+        timeOfDay += Time.deltaTime;
+        if (timeOfDay >= 1)
+        {
+            timeOfDay -= 1;
+        }
+        if (timeOfDay < .5)
+        {
+            sun.intensity -= Time.deltaTime * lightMultiplier;
+        }
+        else
+        {
+            sun.intensity += Time.deltaTime * lightMultiplier;
+        }
+        sun.transform.Rotate(new Vector3(timeOfDay, sun.transform.rotation.y, sun.transform.rotation.z));
     }
 
     public void UpdateValue(string field)
