66c95b07d27c625b7b0464f51f60e63d4f04805c
Daniel <dmforeacre@gmail.com>
2022-11-27 17:35:12 -0500

    Adjusted skin & added torch light effects

--- Assets/Scripts/EnemyHandler.cs
+++ Assets/Scripts/EnemyHandler.cs
@@ -6,10 +6,10 @@ public class EnemyHandler : MonoBehaviour
 {
     public struct enemy
     {
-        GameObject enemyType;
-        int health;
-        int maxHealth;
-        int speed;
+        public GameObject enemyType;
+        public int health;
+        public int maxHealth;
+        public int speed;
 
     }
 
@@ -19,22 +19,30 @@ public class EnemyHandler : MonoBehaviour
     
     private int currentCount = 0;
     private int killedCount = 0;
+    private float lastSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastSpawn = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - lastSpawn > 20)
+        {
+            lastSpawn = Time.time;
+            SpawnEnemy();
+        }
     }
 
     private void SpawnEnemy()
     {
         enemy newEnemy = new enemy();
-        
+        newEnemy.enemyType = Instantiate(basicEnemy);
+        newEnemy.maxHealth = 100;
+        newEnemy.health = newEnemy.maxHealth;
+        newEnemy.speed = 20;
     }
 }
