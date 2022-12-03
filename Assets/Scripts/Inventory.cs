5322a5a8609ba99c1383eecfab6350910eaa14e1
Daniel <dmforeacre@gmail.com>
2022-12-03 12:44:08 -0500

    Added boxes for crafting

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -16,9 +16,13 @@ public class Inventory : MonoBehaviour
         _tools = new Dictionary<string, string>();
 
         _items.Add("Wood", 0);
+        _items.Add("Planks", 0);
         _items.Add("Stone", 0);
-        _items.Add("Copper Ore", 0);
-        _items.Add("Iron Ore", 0);
+        _items.Add("CutStone", 0);
+        _items.Add("CopperOre", 0);
+        _items.Add("CopperBar", 0);
+        _items.Add("IronOre", 0);
+        _items.Add("IronBar", 0);
 
         _tools.Add("sword", "Wood");
         _tools.Add("axe", "Wood");
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,8 +9,7 @@ public class UIManager : MonoBehaviour
 {
     public static Image healthBar;
     public static Image staminaBar;
-    public static TextMeshProUGUI woodCount;
-    public static TextMeshProUGUI stoneCount;
+    public static Dictionary<string, TextMeshProUGUI> counts = new Dictionary<string, TextMeshProUGUI>();
 
     public static Image[] tools = new Image[4];
     public static GameObject inventory;
@@ -22,8 +21,14 @@ public class UIManager : MonoBehaviour
         healthBar = GameObject.Find("MainUI/StatusBar/Bars/Health").GetComponent<Image>();
         staminaBar = GameObject.Find("MainUI/StatusBar/Bars/Stamina").GetComponent<Image>();
 
-        woodCount = GameObject.Find("MainUI/Inventory/WoodCount").GetComponent<TextMeshProUGUI>();
-        stoneCount = GameObject.Find("MainUI/Inventory/StoneCount").GetComponent<TextMeshProUGUI>();
+        counts.Add("Wood", GameObject.Find("MainUI/Inventory/WoodCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("Planks", GameObject.Find("MainUI/Inventory/PlanksCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("Stone", GameObject.Find("MainUI/Inventory/StoneCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("CutStone", GameObject.Find("MainUI/Inventory/CutStoneCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("CopperOre", GameObject.Find("MainUI/Inventory/CopperOreCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("CopperBar", GameObject.Find("MainUI/Inventory/CopperBarCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("IronOre", GameObject.Find("MainUI/Inventory/IronOreCount").GetComponent<TextMeshProUGUI>());
+        counts.Add("IronBar", GameObject.Find("MainUI/Inventory/IronBarCount").GetComponent<TextMeshProUGUI>());
 
         inventory = GameObject.Find("MainUI/Inventory");
         inventory.SetActive(false);
@@ -31,7 +36,13 @@ public class UIManager : MonoBehaviour
         inventoryAction.performed += ctx =>
         {
             UpdateCount("Wood");
+            UpdateCount("Planks");
             UpdateCount("Stone");
+            UpdateCount("CutStone");
+            UpdateCount("CopperOre");
+            UpdateCount("CopperBar");
+            UpdateCount("IronOre");
+            UpdateCount("IronBar");
 
             inventory.SetActive(!inventory.activeSelf);
         };
@@ -53,13 +64,22 @@ public class UIManager : MonoBehaviour
 
     public static void UpdateCount(string field)
     {
-        if (string.Equals(field, "Wood"))
+        counts[field].text = Inventory.GetCount(field).ToString();
+        /*if (string.Equals(field, "Wood"))
         {
             woodCount.text = Inventory.GetCount(field).ToString();
+        }
+        else if (string.Equals(field, "Planks"))
+        {
+            stoneCount.text = Inventory.GetCount(field).ToString();
         }
         else if (string.Equals(field, "Stone"))
         {
             stoneCount.text = Inventory.GetCount(field).ToString();
         }
+        else if (string.Equals(field, "CutStone"))
+        {
+            stoneCount.text = Inventory.GetCount(field).ToString();
+        }*/
     }
 }
