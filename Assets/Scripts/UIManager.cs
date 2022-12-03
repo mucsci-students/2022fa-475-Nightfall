9fd87fee630c7890ec2632e877f9e82e5c1191bc
Daniel <dmforeacre@gmail.com>
2022-12-02 20:14:08 -0500

    Fixing inventory UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public static Image healthBar;
     public static Image staminaBar;
+    public static TextMeshProUGUI woodCount;
+    public static TextMeshProUGUI stoneCount;
+
     public static Image[] tools = new Image[4];
     public static GameObject inventory;
 
@@ -18,11 +22,17 @@ public class UIManager : MonoBehaviour
         healthBar = GameObject.Find("MainUI/StatusBar/Bars/Health").GetComponent<Image>();
         staminaBar = GameObject.Find("MainUI/StatusBar/Bars/Stamina").GetComponent<Image>();
 
+        woodCount = GameObject.Find("MainUI/Inventory/WoodCount").GetComponent<TextMeshProUGUI>();
+        stoneCount = GameObject.Find("MainUI/Inventory/StoneCount").GetComponent<TextMeshProUGUI>();
+
         inventory = GameObject.Find("MainUI/Inventory");
         inventory.SetActive(false);
 
         inventoryAction.performed += ctx =>
         {
+            UpdateCount("Wood");
+            UpdateCount("Stone");
+
             inventory.SetActive(!inventory.activeSelf);
         };
 
@@ -38,6 +48,18 @@ public class UIManager : MonoBehaviour
         else if (string.Equals(field, "stamina"))
         {
             staminaBar.fillAmount = (float)v / (float)maxV;
+        }
+    }
+
+    public static void UpdateCount(string field)
+    {
+        if (string.Equals(field, "Wood"))
+        {
+            woodCount.text = Inventory.GetCount(field).ToString();
+        }
+        else if (string.Equals(field, "Stone"))
+        {
+            stoneCount.text = Inventory.GetCount(field).ToString();
         }
     }
 }
