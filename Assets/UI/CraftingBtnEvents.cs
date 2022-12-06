90353765cadb9e78998aa42635999775e190770a
Daniel <dmforeacre@gmail.com>
2022-12-05 19:06:23 -0500

    Merging

--- Assets/UI/CraftingBtnEvents.cs
+++ Assets/UI/CraftingBtnEvents.cs
@@ -49,39 +49,84 @@ public class CraftingBtnEvents : MonoBehaviour
     }
     public void StoneSword()
     {
-
+        if (Inventory.GetTool("sword") != "Wood" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Wooden Sword to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void StoneAxe()
     {
-
+        if (Inventory.GetTool("axe") != "Wood" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Wooden Axe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void StonePickaxe()
     {
-
+        if (Inventory.GetTool("pickaxe") != "Wood" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Wooden Pickaxe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void CopperSword()
     {
-
+        if (Inventory.GetTool("sword") != "Stone" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Stone Sword to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void CopperAxe()
     {
-
+        if (Inventory.GetTool("axe") != "Stone" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Stone Axe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void CopperPickaxe()
     {
-
+        if (Inventory.GetTool("pickaxe") != "Stone" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Stone Pickaxe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void IronSword()
     {
-
+        if (Inventory.GetTool("sword") != "Copper" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Copper Sword to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void IronAxe()
     {
-
+        if (Inventory.GetTool("axe") != "Copper" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Copper Axe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     public void IronPickaxe()
     {
-
+        if (Inventory.GetTool("pickaxe") != "Copper" && !msgDisplaying)
+        {
+            msgDisplaying = true;
+            UIManager.SetMsgText("You must have a Copper Pickaxe to make this");
+            StartCoroutine(FadeMsg());
+        }
     }
     IEnumerator FadeMsg()
     {
